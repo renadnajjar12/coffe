1ffda52acf363376cba2050e5f4792d749ef0ad6
renadnajj <renadnajj24@gmail.com>
2022-09-20 17:17:18 +0300

    coffe

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let list= document.querySelector("#list");
+let sear=document.querySelector("#sear");
+let search=document.querySelector(".search");
+sear.addEventListener("click",function(){
+    search.style.cssText="display:inline; transition: 1s ease;width: 200px;"
+});
+
+
